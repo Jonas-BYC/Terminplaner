d06845faff9086f715b8d97fd9739c2c4360b24a
Janoxa <janphilipp.albinger@gmail.com>
2025-09-19 12:38:04 +0200

    model

---
+++ model/Appointment.cs
@@ -0,0 +1,11 @@
+class Appointment
+{
+    public string id { get; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public DateTime date { get; set; }
+
+    public Appointment() {
+    }
+    
+}
