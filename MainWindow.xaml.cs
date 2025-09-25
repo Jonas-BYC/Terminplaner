494e28fcb7bce7a5abe8e56b637a500e661b3315
albinger <janphilipp.albinger@encoway.de>
2025-09-25 15:25:58 +0200

    no idea what im doing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
+﻿using System.Windows;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Terminplaner
 {
@@ -17,48 +9,29 @@ namespace Terminplaner
     
     public partial class MainWindow : Window
     {
-        private DateTime _currentDate = new DateTime(2025, 1, 1);
+        
+        private List _appointments = new List();
 
         public MainWindow()
         {
             InitializeComponent();
-            DateDisplayButton.Content = _currentDate.ToString("dd.MM.yyyy");
-            UpdateDateDisplay();
-        }
-
-        private void PreviousDate_Click(object sender, RoutedEventArgs e)
-        {
-            _currentDate = _currentDate.AddDays(-1);
-            UpdateDateDisplay();
-        }
-
-        private void NextDate_Click(object sender, RoutedEventArgs e)
-        {
-            _currentDate = _currentDate.AddDays(1);
-            UpdateDateDisplay();
+            
         }
 
         
-        private void DateDisplay_Click(object sender, RoutedEventArgs e)
+        private void ShowDialogButton_Click(object sender, RoutedEventArgs e)
         {
-            DatePickerControl.Visibility = Visibility.Visible;
-            DatePickerControl.IsDropDownOpen = true;
+            var dialog = new yewah();
+            var result = dialog.ShowDialog();
+            
+            MessageBox.Show(result == true ? "User clicked OK" : "User clicked Cancel");
         }
 
-        private void DatePickerControl_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (DatePickerControl.SelectedDate.HasValue)
-            {
-                _currentDate = DatePickerControl.SelectedDate.Value;
-                UpdateDateDisplay();
-                DatePickerControl.Visibility = Visibility.Collapsed;
-            }
-        }
+        
 
-
-        private void UpdateDateDisplay()
-        {
-            DateDisplayButton.Content = _currentDate.ToString("dd.MM.yyyy");
+        private void createAppointment(object sender, RoutedEventArgs e) {
+            
+            ShowDialogButton_Click(sender, e);  
         }
     }
 
---
+++ yewah.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Terminplaner;
+
+public partial class yewah : Window
+{
+    public bool DialogResultValue { get; private set; }
+
+    public yewah()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Ok_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResultValue = true;
+    }
+
+    private void Cancel_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResultValue = false;
+    }
+
+    public bool? ShowDialog()
+    {
+        return DialogResultValue;
+    }
+}
